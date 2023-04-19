502901d58888ad5ea907915e5037ebff44d5cefd
JPrimee <javierpsantos99@gmail.com>
2023-04-19 09:14:20 +0200

    Creación del proyecto

---
+++ EX01_PruebaTecnica/src/main/java/com/example/demo/Ex01PruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01PruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01PruebaTecnicaApplication.class, args);
+	}
+
+}
